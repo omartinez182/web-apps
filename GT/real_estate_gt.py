5d30c6dfadfb57d2cbed9b438f83a313e0e3ff93
omartinez182 <omartinez@domalabs.com>
2021-02-19 19:21:43 -0600

    Rolled out new filters for bar plots

--- GT/real_estate_gt.py
+++ GT/real_estate_gt.py
@@ -19,7 +19,7 @@ code = """<!-- Google Tag Manager -->
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NLR68SM');</script>
+})(window,document,'script','dataLayer','GTM-XXXXXX');</script>
 <!-- End Google Tag Manager -->"""
 
 a=os.path.dirname(st.__file__)+'/static/index.html'
